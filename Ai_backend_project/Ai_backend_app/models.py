bd73b7fac81c0746c0392c30595039a2f6d58df1
sahil <midriffdeveloper22@gmail.com>
2024-10-07 13:18:40 +0530

    bb

---
+++ Ai_backend_project/Ai_backend_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class Cuser(AbstractUser):
+    phone_no          = models.CharField(max_length=10,unique=True,null=True,blank=True)
+    access            = models.CharField(max_length=2000,unique=True,null=True,blank=True)
+    address           = models.TextField(null=True,blank=True)
+    age               = models.IntegerField(null=True,blank=True)
+
+    def __str__(self):
+        return f"{self.id} -- {self.username}"
