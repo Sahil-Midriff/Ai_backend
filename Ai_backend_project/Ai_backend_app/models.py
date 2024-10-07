30816161224e33f2092073b51653ecda1d57983c
sahil <midriffdeveloper22@gmail.com>
2024-10-07 14:54:54 +0530

    'ff'

--- Ai_backend_project/Ai_backend_app/models.py
+++ Ai_backend_project/Ai_backend_app/models.py
@@ -8,6 +8,7 @@ class Cuser(AbstractUser):
     access            = models.CharField(max_length=2000,unique=True,null=True,blank=True)
     address           = models.TextField(null=True,blank=True)
     age               = models.IntegerField(null=True,blank=True)
+    status            = models.BooleanField(null=True,blank=True)
 
     def __str__(self):
         return f"{self.id} -- {self.username}"
